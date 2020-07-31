1e62513592104b17a5e682a28f7e0267d3f79977
Tanakorn <tanakorncode@gmail.com>
2020-08-01 05:47:36 +0700

    bug fix

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=db;dbname=smart-care;port=3306',
     'username' => 'root',
-    'password' => '',
+    'password' => 'b8888888',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
